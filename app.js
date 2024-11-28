512951ceee687d7ca4e5144eaa32c9c25e090014
xjf255 <juanfher.255@gmail.com>
2024-11-28 08:47:53 -0600

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express, { json } from 'express'
 import { createTasksRouter } from './routes/tasks.js'
+import { corsMiddleware } from './middleware/cors.js'
 
 export const createApp = ({ TaskModel }) => {
   const app = express()
   app.use(json())
   app.disable('x-powered-by')
+  app.use(corsMiddleware())
   app.use("/tasks", createTasksRouter({ TaskModel }))
 
   const PORT = process.env.PORT ?? 1234
